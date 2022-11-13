94b966117fd4d5584234e139e26ef069d1391f7d
Adriana <Adriana@LAPTOP-P9HSBAIV>
2022-11-12 23:05:38 -0500

    Agregar archivos de proyecto.

---
+++ Ej4.1/Program.cs
@@ -0,0 +1,23 @@
+﻿//ejemplos 4
+using System;
+
+namespace Ejercicio4_1
+{
+    class ejercicio
+    {
+        static void Main()
+        {
+            int i = 1;
+            int suma = 0;
+            while (i <= 10)
+            {
+                Console.WriteLine("Digite un numero ({0})", i);
+                int datos = Convert.ToInt32(Console.ReadLine());
+                suma = suma + datos;
+                i++;
+                Console.WriteLine("La suma actual de los numeros es: {0}", suma);
+                Console.WriteLine();
+            }
+        }
+    }
+}
